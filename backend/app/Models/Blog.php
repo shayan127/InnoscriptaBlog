138d5cc7e6f82487876fae3f3de253750b4c6453
shayan127 <superviper007@yahoo.com>
2025-02-23 13:17:05 +0330

    Feat : Create models and migrations

---
+++ backend/app/Models/Blog.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'snippet',
+        'content',
+        'published_at',
+        'source', // todo
+        'authors',
+        'image',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'authors' => 'array', // todo
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'blog_category');
+    }
+}
