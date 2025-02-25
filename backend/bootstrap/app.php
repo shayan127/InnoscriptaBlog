44779c08304c8885b329f93bdcb62c5db141e9c2
shayan127 <superviper007@yahoo.com>
2025-02-25 19:30:40 +0330

    Fix : remove unnecessary things

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Middleware\AuthenticateApi;
-use App\Http\Middleware\HandleApiErrors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
